cd2e1284a9aaf5debbdb813f4828f3e83aef7205
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-11-22 11:39:06 +0100

    feat: feat/page-1

---
+++ src/components/login.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import {
+    InputGroup,
+    InputGroupInput,
+} from "@/components/ui/input-group"
+
+export function Login() {
+    return (
+        <div className="grid w-full max-w-sm gap-6">
+            <InputGroup className="rounded-full mt-5">
+                <InputGroupInput type="email" placeholder="Enter your email" />
+                <button className='bg-orange-500 text-white px-4 py-2 font-bold rounded-full'>Get Started</button>
+            </InputGroup>
+        </div>
+    )
+}
+
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Login } from './login'
 
 export default function Main() {
     return (
         <div className='flex flex-col items-center justify-center mt-32 px-10'>
             <p className='font-bold text-5xl'>To Do List</p>
-            <button className='bg-orange-500 text-white px-6 py-2 font-bold rounded-full text-5xl'>Optimizer</button>
+            <button className='bg-orange-500 text-white px-6 py-2 font-bold rounded-full text-5xl mt-1'>Optimizer</button>
             <p className="text-gray-500 mt-3 text-center">
                 This platform can determin the importance of your tasks,
                 allowing you to be<br />
                 more efficient in your dally operations.
             </p>
+            <Login />
         </div>
     )
 }
