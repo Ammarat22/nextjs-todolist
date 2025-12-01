e96351f93e7d29ecaad38422e03e38c3163832da
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-12-02 00:50:33 +0100

    feat: creat-list

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 border-b'>
+        <p className='font-bold text-2xl'>Todo List</p>
+        <p> <BellDot /><span>avatar</span></p>
+    </div>
+  )
+}
