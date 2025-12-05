ea0ed1fb69d004035acdf6c29208682a029261f1
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-12-05 21:21:14 +0100

    feat: feat/list-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import Header from "@/components/header";
-
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <Header />
       <main className="flex-1 flex items-center justify-center">
         <h1 className="text-3xl font-bold">Welcome to Todo List App</h1>
       </main>
     </div>
-    //  --- IGNORE ---  
-    
   );
 }
