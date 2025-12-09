af93b7b217be7b6d2b4e106d6cee0bdd33dc14c1
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-12-09 12:08:13 +0100

    feat: feat/list-1

--- src/components/sub-main.tsx
+++ src/components/sub-main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from './ui/button'
 
-export default function SubMain() {
+export default function SubMain()  {
   return (
-    <div>
-        Active Task Completed
+    <div className="flex flex-wrap items-center gap-2 md:flex-row p-4">
+      <Button variant="outline">Active Task </Button>
+      <Button variant="outline" aria-label="Submit" className='opacity-65'>
+        Completed
+      </Button>
     </div>
   )
 }
