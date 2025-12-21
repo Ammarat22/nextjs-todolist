028bde8688497f84867ef84b85c872eb32642313
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-12-21 11:53:56 +0100

    add-images

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,10 +4,10 @@ import Main from "@/components/main";
 
 export default function Home() {
   return (
-    <div>
+      <div>
       <Header/>
       <Main/>
       <Footer/>
-    </div>
+      </div>
   );
 }
