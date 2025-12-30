d60b796d8141f084a383a9240237e4d0ec40c135
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-12-30 10:26:20 +0100

    news-changes/images

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -36,11 +36,11 @@ export default function Main() {
                 />
             </div>
             <div className="grid w-full max-w-sm gap-6">
-            <InputGroup className="rounded-full mt-5">
-                <InputGroupInput type="email" placeholder="Enter your email"/>
-                <button type="submit" className='bg-orange-500 text-white px-4 py-1.5 font-bold rounded-full'>Get Started</button>
-            </InputGroup>
-        </div>
+                <InputGroup className="rounded-full mt-5">
+                    <InputGroupInput type="email" placeholder="Enter your email" />
+                    <button type="submit" className='bg-orange-500 text-white px-4 py-1.5 font-bold rounded-full'>Get Started</button>
+                </InputGroup>
+            </div>
         </div>
     )
 }
