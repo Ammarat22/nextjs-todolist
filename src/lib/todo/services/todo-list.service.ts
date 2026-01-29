04c68547c94ab305188f1e1c4dd32fdbde94f4ac
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2026-01-29 13:19:20 +0100

    feat:create-task

--- src/lib/todo/services/todo-list.service.ts
+++ src/lib/todo/services/todo-list.service.ts
@@ -1,16 +1,23 @@
 import { TodoListDto } from "../models/todo-list.model";
-
+import axios from 'axios';
 
 
 const API_URL = 'http://localhost:8080/api/todolist';
 
 export const fetchTodoLists = (order?: string): Promise<TodoListDto[]> => {
   const url = order ? `${API_URL}?order=${order}` : API_URL;
-  return fetch(url).then((res) => res.json());
+  return fetch(url)
+  .then((res) => res.json());
 }
+
 export const getAllTodoLists = async (
-    order?: 'asc' | 'desc',
+  order?: 'asc' | 'desc',
 ): Promise<TodoListDto[]> => {
-  const response = await fetch(API_URL);
-    return response.json();
-}
+  try {
+    const response = await axios.get<TodoListDto[]>(API_URL, { params: { order } });
+    return response.data;
+  } catch (error) {
+    console.error('Erreur getAllTodoLists:', error);
+    return [];
+  }
+};
